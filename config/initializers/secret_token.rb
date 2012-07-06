fe54788bf88e1b0763f56d8ae105fe9038cbb8bc
Vikas Reddy <vikasreddy.com@gmail.com>
2012-07-06 12:06:57 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviceInventory::Application.config.secret_token = '9b03e559dc5fdb56f4ff76d66c87092ca1aefa2be0fffa50f2f26c3ae296318e7fc5dad314550d85be0273670c4de2a3af1e034c2c99715e6e6f61d3eaaff514'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DeviceInventory::Application.config.session_store :cookie_store, key: '_device-inventory_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DeviceInventory::Application.config.session_store :active_record_store
