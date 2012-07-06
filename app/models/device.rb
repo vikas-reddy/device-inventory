921c0e49f55aa012fb2671829c5688e21b432ca3
Keshav <keshav.k@imaginea.com>
2012-07-06 16:48:22 +0530

    Added associations between devices and user.Added Assigned To and Owned By columns

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,4 @@
 class Device < ActiveRecord::Base
   attr_accessible :environment, :ip_addr, :mac_addr, :make, :model, :os, :os_version, :owner_id, :phone_num, :possesser_id, :project, :serial_num, :service_provider, :device_type, :status
+  belongs_to :user
 end
