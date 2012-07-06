bb9175e8c76c9efd7e9a6878b13f6bd0222594fd
Keshav <keshav.k@imaginea.com>
2012-07-06 17:20:30 +0530

    Removed owner_id and possessor_id columns from validate_presence_of

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,5 @@
 class Device < ActiveRecord::Base
   attr_accessible :environment, :ip_addr, :mac_addr, :make, :model, :os, :os_version, :owner_id, :phone_num, :possesser_id, :project, :serial_num, :service_provider, :device_type, :status
-  validates_presence_of :environment, :ip_addr, :mac_addr, :make, :model, :os, :os_version, :owner_id, :phone_num, :possesser_id, :project, :serial_num, :service_provider, :device_type, :status
+  validates_presence_of :environment, :ip_addr, :mac_addr, :make, :model, :os, :os_version,  :phone_num, :project, :serial_num, :service_provider, :device_type, :status
   belongs_to :user
 end
