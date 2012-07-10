1938284a47cfcf05df909470a23de628baddfafd
Vikas Reddy <vikasreddy.com@gmail.com>
2012-07-10 14:34:55 +0530

    Refactor a few UI elements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def accessories_heading(device)
+    count = device.accessories.count
+    if count > 0
+      "Accessories (#{count})"
+    else
+      "Accessories"
+    end
+  end
 end
