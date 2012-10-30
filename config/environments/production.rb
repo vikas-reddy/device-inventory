2fc987d0eebd3e215537bc72fc1b3196d578f300
Anil kumar <anil.r@imaginea.com>
2012-10-30 14:55:01 +0530

    Modified smtp settings as per the changes made by the admin team.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,9 +51,9 @@ DeviceInventory::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { 
-    :address              => 'smtp.imaginea.com',
-    :port                 => 587,
-    :domain               => 'www.pramati.com',
+    :address              => 'smtp.pramati.com',
+    :port                 => 25,
+    :domain               => 'pramati.com',
     :user_name            => 'bmtrac@pramati.com',
     :password             => 'bmtrac',
     :authentication       => :login,
